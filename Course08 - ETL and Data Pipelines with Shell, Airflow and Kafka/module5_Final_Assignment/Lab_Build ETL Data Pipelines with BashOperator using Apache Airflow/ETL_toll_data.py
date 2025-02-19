a7633c1a35223fa74568cfc38a235054d2e79a33
Ercan Tayfun <ercantayfun1@gmail.com>
2025-02-19 23:15:51 +0100

    Course8_module5_part1

---
+++ Course08 - ETL and Data Pipelines with Shell, Airflow and Kafka/module5_Final_Assignment/Lab_Build ETL Data Pipelines with BashOperator using Apache Airflow/ETL_toll_data.py
@@ -0,0 +1,82 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.utils.dates import days_ago
+from datetime import timedelta
+
+#defining DAG arguments
+default_args = {
+    'owner': 'Ercan Tayfun',
+    'start_date': days_ago(0),
+    'email': ['et@somemail.com'],
+    'email_on_failure': True,
+    'email_on_retry': True,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+# define the DAG
+dag = DAG(
+    dag_id='ETL_toll_data',
+    default_args=default_args,
+    description='Apache Airflow Final Assignment',
+    schedule_interval=timedelta(days=1),
+)
+
+# Task1: Unzip the downloaded data
+unzip_data = BashOperator(
+    task_id='unzip_data',
+    bash_command='tar -xvzf /home/project/airflow/dags/finalassignment/tolldata.tgz -C /home/project/airflow/dags/finalassignment',
+    dag=dag,
+)
+
+# Task2: Extract data from vehicle-data.csv
+extract_data_from_csv = BashOperator(
+    task_id='extract_data_from_csv',
+    bash_command='cut -d"," -f1-4 /home/project/airflow/dags/finalassignment/vehicle-data.csv > /home/project/airflow/dags/finalassignment/csv_data.csv',
+    dag=dag,
+)
+
+# Task3: Extract data from  tollplaza-data.tsv
+extract_data_from_tsv = BashOperator(
+    task_id='extract_data_from_tsv',
+    bash_command='tr "\t" "," < /home/project/airflow/dags/finalassignment/tollplaza-data.tsv | cut -d"," -f5-7 > /home/project/airflow/dags/finalassignment/tsv_data.csv',
+    dag=dag,
+)
+
+# Task4: Extract data from payment-data.txt
+extract_data_from_fixed_width = BashOperator(
+    task_id='extract_data_from_fixed_width',
+    bash_command='awk \'{print $10 "," $11}\' /home/project/airflow/dags/finalassignment/payment-data.txt > /home/project/airflow/dags/finalassignment/fixed_width_data.csv',
+    dag=dag,
+)
+
+# Task 5: Consolidate extracted data into a single CSV file ()
+consolidate_data = BashOperator(
+    task_id='consolidate_data',
+    bash_command="""
+    paste -d"," \
+        /home/project/airflow/dags/finalassignment/csv_data.csv \
+        /home/project/airflow/dags/finalassignment/tsv_data.csv \
+        /home/project/airflow/dags/finalassignment/fixed_width_data.csv \
+    > /home/project/airflow/dags/finalassignment/extracted_data.csv
+    """, 
+    dag=dag,
+)
+
+# Task6: Transform the data (convert vehicle_type to uppercase)
+transform_data = BashOperator(
+    task_id='transform_data',
+    bash_command="""awk 'BEGIN {OFS=\",\"} {$4=toupper($4); print}' /home/project/airflow/dags/finalassignment/extracted_data.csv > /home/project/airflow/dags/finalassignment/transformed_data.csv""",
+    dag=dag,
+)
+
+
+# Task7: Load the data (move the transformed file to final directory)
+load_data = BashOperator(
+    task_id='load_data',
+    bash_command='mv /home/project/airflow/dags/finalassignment/transformed_data.csv /home/project/airflow/dags/finalassignment/final_data.csv',
+    dag=dag,
+)
+
+# Define task dependencies
+unzip_data >> [extract_data_from_csv, extract_data_from_tsv, extract_data_from_fixed_width] >> consolidate_data >> transform_data >> load_data
